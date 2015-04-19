0c20fd0fa0d8fb75fd97934a95ac972c9e3e9a6f
Emi Berea <emi.berea@gmail.com>
2015-04-19 19:57:18 +0300

    Added basic classes and interfaces hierarchy for the Business project

---
+++ Business/Managers/IStudentManager.cs
@@ -0,0 +1,19 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Managers
+{
+    public interface IStudentManager
+    {
+        List<Student> GetAll();
+        Student GetOneById(int id);
+        void Add(Student student);
+        void AddList(List<Student> students);
+        void Edit(int id, Student student);
+        void Delete(int id);
+    }
+}
