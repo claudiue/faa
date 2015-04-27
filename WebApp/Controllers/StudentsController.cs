cc5e09d14108f8e11432a5160c26260b603f2e4e
Claudiu Epure <claudiu.epure@gmail.com>
2015-04-27 21:05:19 +0300

    Add WebApp project

---
+++ WebApp/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Business;
+using Business.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private IStudentManager _studentManager;
+        private IAdmissionManager _admissionManager;
+
+        public StudentsController()
+        {
+            _studentManager = BusinessFactory.CreateStudentManager();
+            _admissionManager = BusinessFactory.CreateAdmissionManager();
+        }
+
+        public ActionResult Index()
+        {
+            var students = _studentManager.GetAll();
+            return View(students);
+        }
+    }
+}
